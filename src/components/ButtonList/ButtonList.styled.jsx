c014cdf183469400143c625e1e85fa02618c787f
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-26 04:41:08 +0300

    ok all

--- src/components/ButtonList/ButtonList.styled.jsx
+++ src/components/ButtonList/ButtonList.styled.jsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const Title = styled.h2`
-  text-align: center;
   margin-bottom: ${props => props.theme.spacing(7)};
   padding-right: ${props => props.theme.spacing(7)};
+  font-size: 28px;
+  text-align: center;
+  letter-spacing: 0.1em;
 `;
 
 export const ButtonBox = styled.div`
